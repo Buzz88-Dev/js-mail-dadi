b636ab622cc642c37f8976d228ff1b0ecb41e9bf
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-09 16:32:42 +0200

    aggiornamento

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// lista di email
+let listaMail = ["omar@gmail.com", "silvia@gmail.com", "luca@gmail.com", "roberto@gmail.com"];
+
+let inputMail = document.getElementById("mymail");
+
+let verificaMail = document.getElementById("myButton");
+
+let puoiNonPuoi = document.querySelector(".presenteAssente");
+
+let presente;
+
+let assente;
+
+verificaMail.addEventListener("click",
+    function(){
+
+        let scrivoMail = inputMail.value;
+        console.log(scrivoMail);
+        let i;
+
+        for (i = 0; i < listaMail.length; i++) {   
+            console.log(listaMail[i]);
+
+            if (scrivoMail === listaMail[i]) {
+                presente = 1;
+                console.log("Puoi accedere!!!");
+            } else {
+                presente = 0;
+                console.log("Non puoi");
+            }
+        }        
+    }
+)
